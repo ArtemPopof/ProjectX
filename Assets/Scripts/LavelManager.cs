e7706eaa4817c3104696072e3cef34318a68cd70
Roman.Planidin <Roman.Planidin@x5.ru>
2020-03-15 20:11:53 +0300

    added models and script for spawn

---
+++ Assets/Scripts/LavelManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavelManager : MonoBehaviour
+{
+    public static LavelManager Instance {set; get;}
+
+    //Level spawning
+
+    //List of pieces
+
+    public List<Piece> ramps = new List<Piece>();
+    public List<Piece> longBlocks = new List<Piece>();
+    public List<Piece> jumps = new List<Piece>();
+    public List<Piece> slides = new List<Piece>();
+    public List<Piece> pieces = new List<Piece>();
+    public List<Piece> houses = new List<Piece>();
+
+    public Piece GetPiece(PieceType pieceType, int visualIndex)
+    {
+        Piece piece = pieces.Find(x => x.type == pieceType && x.visialIndex == visualIndex && !x.gameObject.activeSelf);
+
+        if (piece == null)
+        {
+            GameObject go = null;
+            if (pieceType == PieceType.ramp)
+                go = ramps[visualIndex].gameObject;
+            else if (pieceType == PieceType.longBlock)
+                go = longBlocks[visualIndex].gameObject;
+            else if (pieceType == PieceType.jump)
+                go = jumps[visualIndex].gameObject;
+            else if (pieceType == PieceType.slide)
+                go = slides[visualIndex].gameObject;
+            else if (pieceType == PieceType.house)
+                go = houses[visualIndex].gameObject;
+
+            go = Instantiate(go);
+            piece = go.GetComponent<Piece>();
+            pieces.Add(piece);
+        }
+        return piece;
+    }
+}
