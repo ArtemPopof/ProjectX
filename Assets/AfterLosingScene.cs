ea4a5d84676a3625a8d18107e2279e5250fbfe65
Artem Popov <Deviling42@gmail.com>
2020-03-26 23:45:57 +0300

    Working on a surprise feature

---
+++ Assets/AfterLosingScene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterLosingScene : MonoBehaviour
+{
+    public Transform prizes;
+    public Transform closedPrize;
+    private const int chanceToGetNothing = 5;
+    public Transform emptyPrizePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPrizeClick()
+    {
+        closedPrize.gameObject.SetActive(false);
+
+        // generate random prize
+        bool prizeIsEmpty = UnityEngine.Random.Range(0, 100) <= chanceToGetNothing;
+        if (prizeIsEmpty)
+        {
+            GiveEmptyPrize();
+            return;
+        }
+
+        var prizeIndex = UnityEngine.Random.Range(0, prizes.childCount);
+        var prize = prizes.GetChild(prizeIndex);
+        prize.gameObject.SetActive(true);
+    }
+
+    private void GiveEmptyPrize()
+    {
+        emptyPrizePanel.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Prize.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prize : MonoBehaviour
+{
+    public int maxCount;
+    public int chance;
+}
+
+public enum PrizeType
+{
+    Coins,
+    Nothing
+}
