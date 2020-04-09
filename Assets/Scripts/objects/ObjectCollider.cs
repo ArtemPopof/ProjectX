aaeca4057726647d0c658793e664490a0d2e6ed1
Artem Popov <Deviling42@gmail.com>
2020-04-09 23:00:56 +0300

    Two more segments and fixes =)

---
+++ Assets/Scripts/objects/ObjectCollider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollider : MonoBehaviour
+{
+    public bool isDebugMode = true;
+
+    void Start()
+    {
+        if (!isDebugMode)
+        {
+            removeDebugRenderingComponent();
+        }
+    }
+
+    private void removeDebugRenderingComponent()
+    {
+        GetComponent<MeshRenderer>().gameObject.SetActive(false);
+    }
+}
