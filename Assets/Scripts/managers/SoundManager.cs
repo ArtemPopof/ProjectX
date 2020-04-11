0a6bbcd3c94af45336045667ee0c19d8fab4823c
Artem Popov <Deviling42@gmail.com>
2020-04-11 16:30:33 +0300

    added coin music)

--- Assets/Scripts/managers/SoundManager.cs
+++ Assets/Scripts/managers/SoundManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,12 @@ public class SoundManager : MonoBehaviour
 {
     public static AudioClip playerDeath, playerJump, coin, swipe1, swipe2;
     static AudioSource audioSource;
+    private static int coinsCountInARow;
+    private static float lastCoinTime;
+    private static float maxCoinStreakDelay = 0.8f;
+    private const float HALF_STEP_PITCH_INCREASE = 1.0f / 12.0f;
+    private static int[] HALF_STEP_PATTERN = { 3, 2, 2 };
+
     void Start()
     {
         playerDeath = Resources.Load<AudioClip>("broken");
@@ -15,33 +22,74 @@ public class SoundManager : MonoBehaviour
         swipe2 = Resources.Load<AudioClip>("swipe2");
 
         audioSource = GetComponent<AudioSource>();
+        lastCoinTime = 0;
+        coinsCountInARow = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public static void PlaySound(string clip)
     {
-        switch(clip) {
+        resetPitch();
+
+        switch (clip) {
             case "Death":
-            audioSource.PlayOneShot(playerDeath);
-            break;
+                audioSource.PlayOneShot(playerDeath);
+                break;
             case "Jump":
-            audioSource.PlayOneShot(playerJump);
-            break;
+                audioSource.PlayOneShot(playerJump);
+                break;
             case "Coin":
-            audioSource.PlayOneShot(coin);
+                increaseAudioPitch();
+                audioSource.PlayOneShot(coin);
             break;
-            case "Swipe1":
-            audioSource.PlayOneShot(swipe1);
-            break;
+                case "Swipe1":
+                audioSource.PlayOneShot(swipe1);
+                break;
             case "Swipe2":
-            audioSource.PlayOneShot(swipe2);
-            break;
-            
+                audioSource.PlayOneShot(swipe2);
+                break;
+            default:
+                break;
         }
+    }
+
+    private static void resetPitch()
+    {
+        audioSource.pitch = 1;
+    }
+
+    private static void increaseAudioPitch()
+    {
+        if (coinStreakIsOver())
+        {
+            coinsCountInARow = 0;
+        }
+
+        lastCoinTime = Time.time;
+
+        var pitch = calculateCurrentPitch();
+        Debug.Log("Current pitch: " + pitch);
+        audioSource.pitch = pitch;
+    }
+
+    private static float calculateCurrentPitch()
+    {
+        int currentPatternIndex = coinsCountInARow++ / 4 % HALF_STEP_PATTERN.Length;
+        int totalHalfstepsFromRoot = 0;
+        for (int i = 0; i < currentPatternIndex; i++)
+        {
+            totalHalfstepsFromRoot += HALF_STEP_PATTERN[i];
+        }
+        return 1.0f + totalHalfstepsFromRoot * HALF_STEP_PITCH_INCREASE;
+    }
+
+    private static bool coinStreakIsOver()
+    {
+        return Time.time - lastCoinTime > maxCoinStreakDelay;
     }
 }
