8067545d2d1bb70f7e9b2aa8efa03d6f3d07c119
Artem Popov <Deviling42@gmail.com>
2020-04-22 21:31:28 +0300

    Death ui improvements

---
+++ Assets/Scripts/managers/AdComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdComponent : MonoBehaviour
+{
+    public void ShowAdToResume()
+    {
+        if (!Advertisement.IsReady())
+        {
+            return;
+        }
+
+        Advertisement.Show();
+    }
+}
