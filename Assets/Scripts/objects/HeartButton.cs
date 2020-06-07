3ac4762f0d45320070e2f995c6dcca2d6473baf3
Romario <roma.planidin.96@mail.ru>
2020-06-07 20:30:57 +0300

    added heart to ui

--- Assets/Scripts/objects/HeartButton.cs
+++ Assets/Scripts/objects/HeartButton.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class HeartButton : MonoBehaviour
 {
-    public GameObject heartButton;
-
-        public void DisableHeartButton()
+    public void DisableHeartButton()
     {
         if (PlayerPrefs.GetInt("heart") == 0 || Heart.Instance.IsHeartUsedThreeTimes())
         {
-            heartButton.SetActive(false);
+            gameObject.SetActive(false);
         }
         else
         {
--- Assets/Scripts/ui/GameMenuUI.cs
+++ Assets/Scripts/ui/GameMenuUI.cs
@@ -7,15 +7,18 @@ public class GameMenuUI : MonoBehaviour
 {
     public Text highscore;
     public Text coins;
-
     public Text hearts;
+
+    public int i;
 
     // Start is called before the first frame update
     void Start()
     {
         highscore.text = PlayerPrefs.GetInt("highscore").ToString();
-        coins.text = PlayerPrefs.GetInt("coins").ToString();
         hearts.text = PlayerPrefs.GetInt("heart").ToString();
+        coins.text = PlayerPrefs.GetInt("coins").ToString();
+        
+        i= PlayerPrefs.GetInt("highscore");
     }
 
 }
