607c3d1144bbea42781b4e4f4ea628902afd6a42
Artem Popov <Deviling42@gmail.com>
2020-06-07 16:50:47 +0300

    Working on Levels UI

---
+++ Assets/LevelsShop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelsShop : MonoBehaviour
+{
+    public List<Sprite> levelCovers;
+    private Sprite currentSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentLevel = PlayerPrefs.GetInt("currentLevel");
+        currentSprite = levelCovers[currentLevel];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
