c03f83cf8b21899aaa4fed5b2179f3ff61692b52
Artem Popov <Deviling42@gmail.com>
2020-06-09 23:43:31 +0300

    Improving shop

---
+++ Assets/Level.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour
+{
+    public string name;
+    public int price;
+    public Image shopCover;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
