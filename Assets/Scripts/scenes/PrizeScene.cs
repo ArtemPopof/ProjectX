a1be939dfb84c96863630d5c1b2f3bd57df3f6aa
Artem Popov <Deviling42@gmail.com>
2020-06-30 20:06:29 +0300

    all prize scenes refactoring

--- Assets/Scripts/scenes/PrizeScene.cs
+++ Assets/Scripts/scenes/PrizeScene.cs
@@ -1,23 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PrizeScene : MonoBehaviour
 {
+    public static string CURRENT_PRIZE = "currentPrizeScene";
     public Transform prizes;
     public Transform closedPrize;
-    private const int chanceToGetNothing = 5;
+    public Text prizeDescriptionText;
     public Transform emptyPrizePanel;
     public Transform receivedPrizeLabel;
     public Text yayLabel;
-    private bool open = false;
 
-    public PrizeType prize;
+    public GameObject chestSceneObjects;
+    public GameObject letterSceneObjects;
+    public GameObject eggSceneObjects;
+
+    private bool open = false;
+    private PrizeType type;
 
     // Start is called before the first frame update
     void Start()
     {
+        type = (PrizeType) PlayerPrefs.GetInt(CURRENT_PRIZE);
+        var description = GetPrizeDescriptionText(type);
+        prizeDescriptionText.text = description;
+
+        CustomizeSceneForType(type);
+    }
+
+    private string GetPrizeDescriptionText(PrizeType type)
+    {
+        switch (type)
+        {
+            case PrizeType.CHEST:
+                return "You found a chest";
+            case PrizeType.EGG_QUEST:
+                return "All eggs collected!";
+            case PrizeType.LETTER_QUEST:
+                return "All letters collected!";
+        }
+
+        throw new InvalidOperationException();
+    }
+
+    private void CustomizeSceneForType(PrizeType type)
+    {
+        switch (type)
+        {
+            case PrizeType.CHEST:
+                chestSceneObjects.SetActive(true);
+                break;
+            case PrizeType.EGG_QUEST:
+                eggSceneObjects.SetActive(true);
+                break;
+            case PrizeType.LETTER_QUEST:
+                letterSceneObjects.SetActive(true);
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -77,12 +119,12 @@ public class PrizeScene : MonoBehaviour
     private void GiveMaxPrize() 
     {
 
-        var prizeIndex = Random.Range(0, prizes.childCount);
+        var prizeIndex = UnityEngine.Random.Range(0, prizes.childCount);
         var prize = prizes.GetChild(prizeIndex);
         prize.gameObject.SetActive(true);
 
         var maxPrizeCount = GetMaxPrizeCount(prize);
-        var prizeCount = Random.Range((int) (0.1 * maxPrizeCount), maxPrizeCount);
+        var prizeCount = UnityEngine.Random.Range((int) (0.1 * maxPrizeCount), maxPrizeCount);
         prizeCount = Mathf.Max(1, prizeCount);
 
         GivePrize(prize.GetComponent<Prize>(), prizeCount);
