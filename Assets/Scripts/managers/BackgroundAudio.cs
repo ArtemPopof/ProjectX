26ef53ade16a131de2b910f51769b04932a2809c
Planidin Roman <roma.planidin.96@mail.ru>
2020-07-07 00:32:38 +0300

    added Backgound script and new songs

---
+++ Assets/Scripts/managers/BackgroundAudio.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudio : MonoBehaviour
+{
+    public AudioSource firstLevel;
+    public AudioSource seceondLevel;
+
+    [System.Obsolete]
+    void Start()
+    {
+        var currentLevel = PlayerPrefs.GetInt("currentLevel") - 100;
+        if (currentLevel == 0)
+        {
+            firstLevel.Play();
+        }
+        else 
+        {
+            seceondLevel.Play();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool f = firstLevel.isPlaying;
+        bool s = seceondLevel.isPlaying;
+        var currentLevel = PlayerPrefs.GetInt("currentLevel") - 100;
+        if (currentLevel == 0)
+        {
+            seceondLevel.Stop();
+            firstLevel.Play();
+        } 
+        else
+        { 
+            firstLevel.Stop();
+            seceondLevel.Play();
+        }
+    }
+}
