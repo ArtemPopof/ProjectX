2d751c0ff3f1aec8b39ca9d912b969e66c191978
Artem Popov <Deviling42@gmail.com>
2020-07-23 20:21:48 +0300

    last minute fixes

--- Assets/Scripts/scenes/NewRecordScene.cs
+++ Assets/Scripts/scenes/NewRecordScene.cs
@@ -12,12 +12,12 @@ public class NewRecordScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SoundManager.PlaySound("Highscore");
-
         var score = PlayerPrefs.GetInt("highscore");
         scoreText.text = score.ToString();
 
-        var animator = player.CurrentModel.GetComponentInChildren<Animator>();
+        SoundManager.PlaySound("Highscore");
+
+        //var animator = player.CurrentModel.GetComponentInChildren<Animator>();
         // dragon animation makes dragon run away from camera. 
         // should fix animation before uncommenting this line
         //animator.SetTrigger("StartRunning");
